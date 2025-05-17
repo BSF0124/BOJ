44d99bbff39a5fe12dc4d1c020ff5605cdbc758d
Yunjae Na <150145660+BSF0124@users.noreply.github.com>
2025-05-17 18:30:46 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2800 KB -BaekjoonHub

---
+++ C++17/백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+long long arr[100002];
+
+int main(void) 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr+n);
+
+    int st = 0, ans_st = 0;
+    int en = n-1, ans_en = n-1;
+    int tmp = INT_MAX;
+
+    while(st < en)
+    {
+        int sum = arr[st] + arr[en];
+        if(abs(sum) < tmp)
+        {
+            tmp = abs(sum);
+            ans_st = st;
+            ans_en = en;
+        }
+
+        if(sum < 0) st++;
+        else en--;
+    }
+
+    cout << arr[ans_st] << ' ' << arr[ans_en];
+}
