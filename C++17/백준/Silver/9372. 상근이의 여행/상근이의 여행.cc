95f0675fc1a17cfb2fa2f1c712d54f2670dc18d9
Yunjae Na <150145660+BSF0124@users.noreply.github.com>
2025-07-23 12:21:13 +0900

    [Silver IV] Title: 상근이의 여행, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, m, a, b;
+        cin >> n >> m;
+        while(m--) cin >> a >> b;
+        cout << n-1 << '\n';
+    }
+}
