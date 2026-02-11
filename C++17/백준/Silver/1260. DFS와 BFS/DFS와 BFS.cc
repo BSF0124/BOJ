1644a0e20a55388ad5e2b12e19492c26465dafc4
Yunjae Na <150145660+BSF0124@users.noreply.github.com>
2026-02-11 15:15:29 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2312 KB -BaekjoonHub

--- C++17/백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ C++17/백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,25 +1,24 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
 using namespace std;
 
-int n, m, v;
+int n, m, st;
 vector<int> adj[1002];
-bool vis[1002] = {};
+bool vis[1002];
 
 void bfs()
 {
     queue<int> q;
-    q.push(v);
-    vis[v] = 1;
-
+    q.push(st);
+    vis[st] = 1;
     while(!q.empty())
     {
-        int cur = q.front(); q.pop();
+        auto cur = q.front(); q.pop();
         cout << cur << ' ';
         for(auto nxt : adj[cur])
         {
             if(vis[nxt]) continue;
-            q.push(nxt);
             vis[nxt] = 1;
+            q.push(nxt);
         }
     }
 }
@@ -35,24 +34,24 @@ void dfs(int cur)
     }
 }
 
-int main(void)
+int main() 
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    cin >> n >> m >> v;
+    cin >> n >> m >> st;
     while(m--)
     {
-        int a, b;
-        cin >> a >> b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
 
     for(int i = 1; i <= n; i++)
         sort(adj[i].begin(), adj[i].end());
 
-    dfs(v);
+    dfs(st);
     fill(vis, vis+1002, 0);
     cout << '\n';
     bfs();
